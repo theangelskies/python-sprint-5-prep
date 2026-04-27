bf600429b18e2cc73f80e38be4adcbfe498e8307
theangelskies <theangelskies13@yahoo.com>
2026-04-27 14:56:41 +0200

    classes.py

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name: str, age: int, preferred_operating_system: str):
+        self.name = name
+        self.age = age
+        self.preferred_operating_system = preferred_operating_system
+
+imran = Person("Imran", 22, "Ubuntu")
+print(imran.name)
+print(imran.address)
+
+eliza = Person("Eliza", 34, "Arch Linux")
+print(eliza.name)
+print(eliza.address)
