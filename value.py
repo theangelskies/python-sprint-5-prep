d6a90826493485d445be18794d7a6eff9f4dbf2a
theangelskies <theangelskies13@yahoo.com>
2026-04-27 16:56:20 +0200

    refactor value.py

--- value.py
+++ value.py
@@ -7,3 +7,6 @@ def double(value):
 def second(value):
     return value[1]
 
+#     ✍️exercise
+# Predict what double("22") will do. Then run the code and check. Did it do what you expected? Why did it return the value it did?
+
