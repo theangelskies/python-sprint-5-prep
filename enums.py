28be10d4a557decc165e18dfa177450d394d9752
theangelskies <theangelskies13@yahoo.com>
2026-04-27 17:10:12 +0200

    refactor  enums

--- enums.py
+++ enums.py
@@ -46,3 +46,12 @@ laptops = [
 for person in people:
     possible_laptops = find_possible_laptops(laptops, person)
     print(f"Possible laptops for {person.name}: {possible_laptops}")
+
+#     ✍️exercise
+# Write a program which:
+
+# Already has a list of Laptops that a library has to lend out.
+# Accepts user input to create a new Person - it should use the input function to read a person’s name, age, and preferred operating system.
+# Tells the user how many laptops the library has that have that operating system.
+# If there is an operating system that has more laptops available, tells the user that if they’re willing to accept that operating system they’re more likely to get a laptop.
+# You should convert the age and preferred operating system input from the user into more constrained types as quickly as possible, and should output errors to stderr and terminate the program with a non-zero exit code if the user input bad values.
