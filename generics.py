21677c7f56dc0d6a47db4bf14ae9a191fd9ebaab
theangelskies <theangelskies13@yahoo.com>
2026-04-28 13:30:28 +0200

    fix: add age attribute to Person instances in family tree

--- generics.py
+++ generics.py
@@ -4,12 +4,13 @@ from typing import List
 @dataclass(frozen=True)
 class Person:
     name: str
+    age: int
     children: List["Person"]
 
-fatma = Person(name="Fatma", children=[])
-aisha = Person(name="Aisha", children=[])
+fatma = Person(name="Fatma", age=11, children=[])
+aisha = Person(name="Aisha", age=7, children=[])
 
-imran = Person(name="Imran", children=[fatma, aisha])
+imran = Person(name="Imran", age=43, children=[fatma, aisha])
 
 def print_family_tree(person: Person) -> None:
     print(person.name)
